cda8ba550a8858e0f77a9cf78f96a9ac8815ba42
turtle <super.ivannikov2013@yandex.ru>
2024-12-17 05:32:13 +0300

    Another fix for Jenkins

--- src/test/java/gatling_exec/ExecutorTest.java
+++ src/test/java/gatling_exec/ExecutorTest.java
@@ -3,10 +3,11 @@ package gatling_exec;
 import gatling_exec.utils.wiremock.WireMockSetup;
 import io.gatling.app.Gatling;
 import io.gatling.core.config.GatlingPropertiesBuilder;
+import org.junit.jupiter.api.Test;
 
-public class Executor {
-    public static void main(String[] args) {
-
+class ExecutorTest {
+    @Test
+    public void runGatlingTest() {
         WireMockSetup.startWireMock();
 
         GatlingPropertiesBuilder props = new GatlingPropertiesBuilder()
@@ -19,4 +20,3 @@ public class Executor {
         WireMockSetup.stopWireMock();
     }
 }
-
